e5f709fedf662fb0097b7c0074edebdc96a41a9e
CHARITAH <102732524+CHARITAH@users.noreply.github.com>
2022-11-25 17:56:18 +0530

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -22,11 +22,40 @@ function send(){
     actual_answer = parseInt(number1) * parseInt(number2);
 
     question_number = "<h4>" + number1 + "X" + number2 + "</h4>";
-    input_box = "<br>Answer : <input type= 'text' id= 'input_check_box'>";
+    input_box = "<br>Answer : <input type= 'text' id= 'input_player'>";
     check_button = "<br><br><button class = 'btn btn-primary' onclick = 'check()'>check</button>";
     row = question_number + input_box + check_button ;
     document.getElementById("output").innerHTML = row;
     document.getElementById("number1").value = "";
     document.getElementById("number2").value = "";
    
+}
+
+question_turn = "player1";
+answer_turn = "player2";
+
+function check(){
+    get_answer = document.getElementById("input_check_box").value;
+    if(get_answer == actual_answer)
+    {
+        update_player1_score = update_player1_score +1;
+        document.getElementById("player1_score").innerHTML = update_player;
+    }
+    else
+    {
+        update_player2_score = player2_score+1;
+        document.getElementById("player2_score").innerHTML = update_player2_score;
+    }
+
+}
+
+if(question_turn == "player1")
+{
+    question_turn = "player2"
+    document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name;
+}
+else 
+{
+    question_turn = "player1"
+    document.getElementById("player_question").innerHTML = "Question Turn -" + player1_name;
 }
